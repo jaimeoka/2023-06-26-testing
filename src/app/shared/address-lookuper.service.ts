6adee362cbcee250608aa1b95bc8ab800d1c8197
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-29 20:41:52 +0200

    use addressLookuper

--- src/app/shared/address-lookuper.service.ts
+++ src/app/shared/address-lookuper.service.ts
@@ -1,20 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AddressLookuper {
-  #addresses: string[];
+  #httpClient = inject(HttpClient);
 
-  constructor(addressSupplier: () => string[]) {
-    this.#addresses = addressSupplier();
-  }
   #counter = 0;
 
   get counter() {
     return this.#counter;
   }
 
-  lookup(query: string): boolean {
+  lookup(query: string): Observable<boolean> {
     this.#counter++;
-    return this.#addresses.some((address) => query.startsWith(address));
+    return this.#httpClient
+      .get<undefined[]>('https://nominatim.openstreetmap.org/search.php', {
+        params: new HttpParams().set('q', query).set('format', 'jsonv2'),
+      })
+      .pipe(map((addresses) => addresses.length > 0));
   }
 }
