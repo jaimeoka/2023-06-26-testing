f465fb9966ad4cf36615a90baa410195f5be741b
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-05-29 18:24:09 +0200

    add missing holiday feature

--- e2e/src/support/commands.ts
+++ e2e/src/support/commands.ts
@@ -3,9 +3,22 @@ declare namespace Cypress {
   interface Chainable<Subject> {
     testid(selector: string): Chainable<JQuery<HTMLElement>>;
     findByRole(role: string, textMatch: string | RegExp): Chainable<JQuery>;
+    holidayCards(): Chainable<JQuery<HTMLElement>>;
   }
 }
 
 Cypress.Commands.add('testid', (selector: string) =>
   cy.get(`[data-testid=${selector}]`)
+);
+
+Cypress.Commands.add('holidayCards', () =>
+  cy.get(`[data-testid=holiday-card]`).should((cards) => {
+    expect(cards).to.have.length.greaterThan(0);
+    const allImagesLoaded = cards
+      .find('img')
+      .toArray()
+      .every((img) => (img as HTMLImageElement).naturalWidth > 0);
+
+    expect(allImagesLoaded).to.be.true;
+  })
 );
--- src/app/holidays/holidays/holidays.component.ts
+++ src/app/holidays/holidays/holidays.component.ts
@@ -2,7 +2,20 @@ import { Component, inject } from '@angular/core';
 import { HolidayCardComponent } from '../holiday-card/holiday-card.component';
 import { AsyncPipe, NgForOf } from '@angular/common';
 import { HolidaysRepository } from '../+state';
+import { concatMap, delay, filter, map } from 'rxjs/operators';
+import { concat, first, of } from 'rxjs';
+import { createHoliday } from '@app/holidays/model';
+import { ImagesLoadedService } from '@app/shared';
+import { toSignal } from '@angular/core/rxjs-interop';
 
+const hiddenVienna = createHoliday({
+  id: -1,
+  title: 'Hidden Vienna',
+  teaser: 'Secret Holiday Unlocked',
+  imageUrl: 'https://api.eternal-holidays.net/holiday/vienna.jpg',
+  description:
+    'Congratulations, your patience paid off. You have discovered our Easter egg.',
+});
 @Component({
   selector: 'app-holidays',
   template: `
@@ -19,5 +32,25 @@ import { HolidaysRepository } from '../+state';
   imports: [HolidayCardComponent, NgForOf, AsyncPipe],
 })
 export class HolidaysComponent {
-  holidays = inject(HolidaysRepository).holidays;
+  #imagesLoadedService = inject(ImagesLoadedService);
+
+  holidays = toSignal(
+    inject(HolidaysRepository).holidays$.pipe(
+      concatMap((holidays) =>
+        holidays.length
+          ? concat(
+              of(holidays),
+
+              this.#imagesLoadedService.loaded$.pipe(
+                filter(Boolean),
+                map(() => [...holidays, hiddenVienna]),
+                delay(1000),
+                first()
+              )
+            )
+          : of(holidays)
+      )
+    ),
+    { requireSync: true }
+  );
 }
