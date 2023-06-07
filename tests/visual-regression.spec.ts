b848d04ada97591e36ae7e1ccaa15d7d471d8036
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-06-08 01:19:54 +0200

    add vr test

--- tests/visual-regression.spec.ts
+++ tests/visual-regression.spec.ts
@@ -1,5 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('playwright check', async ({ page }) => {
-  await page.goto('https://www.orf.at');
-});
+for (const story of [
+  'default',
+  'minimal',
+  'overflown',
+  'sold-out',
+  'empty',
+  'tiny-image',
+  'on-sale',
+  'sale-and-sold',
+]) {
+  test(`visual regression for ${story}`, async ({ page }) => {
+    await page.goto(
+      `http://localhost:4400/iframe.html?id=eternal-holiday-card--${story}&viewMode=story`
+    );
+    await expect(page).toHaveScreenshot();
+  });
+}
